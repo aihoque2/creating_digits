934d42652a13d8cb519c1821702ad35bbaa417a9
aihoque2 <aihoque2@illinois.edu>
2023-10-27 18:15:10 -0700

    added skeleton structures for models

--- model.py
+++ model.py
@@ -4,3 +4,18 @@ from torch import nn
 import math
 import matplotlib.pyplot as plt
 
+torch.manual_seed(111)
+
+class Discriminator(nn.Module):
+    def __init__(self):
+        pass
+
+    def forward(self, x):
+        pass
+
+class Generator(nn.Module):
+    def __init__(self):
+        pass
+
+    def forward(self, x):
+        pass
--- train.py
+++ train.py
@@ -1,7 +1,18 @@
+import torch
+from torch import nn
+
 from model import *
 
 """
 train.py
 
-this file is meant to be where the training process is run
+this file is meant to be where the Generator 
+and Discriminator have their zero-sum game 
 """
+
+class Trainer:
+    def __init__(self, generator: nn.Module, discriminator: nn.Module):
+        pass
+
+    def train(self):
+        pass
