30503194593c9536e429f63cfbb10e8d1ea7066a
aihoque2 <aihoque2@illinois.edu>
2023-11-01 17:54:07 -0700

    train.py skeleton and some constructor work

--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 import torch
 from torch import nn
+import torch.optim as optim
 
 from model import *
 
@@ -11,10 +12,34 @@ and Discriminator have their zero-sum game
 """
 
 class Trainer:
-    def __init__(self, generator: nn.Module, discriminator: nn.Module, n_epochs: int):
+
+    def __init__(self, generator: nn.Module, discriminator: nn.Module, n_epochs: int, lr: float, z_dim: int = 100):
+    
         self.generator = generator
         self.discriminator = discriminator
         self.n_epochs = n_epochs
+        self.z_dim = z_dim
+
+        self.criterion = nn.BCELoss()
+
+        self.lr = lr
+        self.G_optimizer = optim.Adam(self.generator.parameters(), lr=self.lr)
+        self.D_optimizer = optim.Adam(self.discriminator.parameters(), lr=self.lr) 
 
     def train(self):
+        """
+        full training process       
+        """
+        pass
+
+    def train_disc(self):
+        """
+        training step of Discriminator model
+        """
+        pass
+
+    def train_gen(self):
+        """
+        training step of Generator model
+        """
         pass
