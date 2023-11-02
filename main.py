630480cb3289681afda69e9dcb9513d9fc48b732
aihoque2 <aihoque2@illinois.edu>
2023-11-01 18:48:07 -0700

    begin work on main method

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
 from model import *
+
+import torch
+from torch import nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.autograd import Variable
+
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+
+def test_model(generator: nn.Module):
+    pass
+
+if __name__=="__main__":
+    generator = Generator()
+    discriminator = Discriminator()
+    print("I did the  main method!")
